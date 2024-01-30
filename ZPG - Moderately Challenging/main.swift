5dae39af987772ecb113c2833ab6b89d02ec4e71
Matt Krause <mkrause@dasd.org>
2024-01-30 08:18:00 -0500

    Initial Commit

--- ZPG - Moderately Challenging/main.swift
+++ ZPG - Moderately Challenging/main.swift
@@ -1,11 +1,32 @@
 //
 //  main.swift
-//  ZPG - Moderately Challenging
+//  Zero Player Game
 //
-//  Created by Krause, Matthew on 1/30/24.
+//  Created by admin on 11/19/19.
+//  Copyright © 2019 admin. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+print("Welcome to the Dungeon!")
 
+// HERO SETUP
+// setup array of heroes
+var heroes : [Adventurer] = []
+var bluto = Warrior()
+bluto.name = "Bluto"
+heroes.append(bluto)
+
+var blammo = Mage()
+blammo.name = "Blammo"
+heroes.append(blammo)
+
+var blippo = Rogue()
+blippo.name = "Blippo"
+heroes.append(blippo)
+
+blippo.status()
+bluto.status()
+blammo.status()
+//select a hero to go
+print("The hero enters the dungeon!")
